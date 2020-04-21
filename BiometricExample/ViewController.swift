bcdc7275f5a6044c775d742ea6f121777988673c
Tom Bates <tom.bates@pushdr.com>
2020-04-21 07:40:51 +0100

    Initial Commit

---
+++ BiometricExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BiometricExample
+//
+//  Created by Tom Bates on 21/04/2020.
+//  Copyright © 2020 Push Doctor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
